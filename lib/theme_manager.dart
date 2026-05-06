27194f21c4acd5e30b9af7d829697fe5749e8876
Mark Joshua Garcia <gmarkjoshua882@gmail.com>
2026-05-06 15:30:23 +0800

    Delete lib directory

--- lib/theme_manager.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-// The "Brain" for Dark Mode
-final ValueNotifier<bool> isDarkModeNotifier = ValueNotifier<bool>(false);
-
-// The "Brain" for Notifications
-final ValueNotifier<bool> isNotificationsNotifier = ValueNotifier<bool>(true);
